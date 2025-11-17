ebb87bdeed5d33320a27b9777a6f8a9c59c349a5
salima <saliima123ss@gmail.com>
2025-11-17 15:58:47 +0500

    9th task on devcore

---
+++ dev-core-101/dev-core-101-basic-syntax.js
@@ -0,0 +1,19 @@
+// Объявляем две переменные
+let a = 10;
+let b = 5;
+
+// Сложение
+let resultAdd = a + b;
+console.log("Сложение:", resultAdd); // 15
+
+// Вычитание
+let resultSub = a - b;
+console.log("Вычитание:", resultSub); // 5
+
+// Умножение
+let resultMul = a * b;
+console.log("Умножение:", resultMul); // 50
+
+// Деление
+let resultDiv = a / b;
+console.log("Деление:", resultDiv); // 2
---
+++ dev-core-101/dev-core-101-basic-syntax.py
@@ -0,0 +1,19 @@
+# Объявляем две переменные
+a = 10
+b = 5
+
+# Сложение
+result_add = a + b
+print("Сложение:", result_add)  # 15
+
+# Вычитание
+result_sub = a - b
+print("Вычитание:", result_sub)  # 5
+
+# Умножение
+result_mul = a * b
+print("Умножение:", result_mul)  # 50
+
+# Деление
+result_div = a / b
+print("Деление:", result_div)  # 2.0
