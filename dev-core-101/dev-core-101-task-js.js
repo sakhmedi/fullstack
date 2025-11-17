1e89c6fc641a5497664a000764ae53da3533fca2
salima <saliima123ss@gmail.com>
2025-11-17 16:11:02 +0500

    10th task on devcore

---
+++ dev-core-101/dev-core-101-task-js.js
@@ -0,0 +1,8 @@
+console.log("1. Choose a movie.");
+console.log("2. Prepare a comfortable place (sofa, bed, or chair).");
+console.log("3. Prepare a snack or a drink.");
+console.log("4. Open the streaming app or website.");
+console.log("5. Find the chosen movie.");
+console.log("6. Press the 'Play' button.");
+console.log("7. Watch the movie until the end.");
+console.log("8. Turn off the screen or close the app.");
